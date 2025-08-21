b13a0b3a7fb0b35c58cae124f4db07b91d753b78
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-08-20 23:03:13 -0500

    v-how

--- src/components/componenteCondiciconal.vue
+++ src/components/componenteCondiciconal.vue
@@ -2,9 +2,18 @@
   <div>
     <h2>Condicionales</h2>
     <div>
-      <h3>v-if</h3>
+      <h3>v-show</h3>
+      <button @click="changeVisible">alterar</button>
+      <p v-show="visible">Hola soy visible con v*show</p>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const visible = ref(true)
+const changeVisible = () => {
+  visible.value = !visible.value
+}
+</script>
 <style scoped></style>
