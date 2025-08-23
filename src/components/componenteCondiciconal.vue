88b4e0177ff5b5d0eddedc56644b7f98c4290297
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-08-23 12:39:37 -0500

    subir una practica

--- src/components/componenteCondiciconal.vue
+++ src/components/componenteCondiciconal.vue
@@ -11,6 +11,11 @@
       <p v-if="visible">Esto es condicional if</p>
       <p v-else>esto es else if</p>
     </div>
+    <div>
+      <button v-if="loqueado" @click="loqueado = false">Iniciar sesion</button>
+      <button v-else @click="loqueado = true">cerara sesion</button>
+      <p v-if="loqueado">Bienvenido al iniciar sesion</p>
+    </div>
   </div>
 </template>
 <script setup>
@@ -20,5 +25,6 @@ const visible = ref(true)
 const changeVisible = () => {
   visible.value = !visible.value
 }
+const loqueado = ref(false)
 </script>
 <style scoped></style>
