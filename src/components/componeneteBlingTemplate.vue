2b6549f409a6ea7ac70e264ccf16c237874d61bd
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-08-25 22:27:15 -0500

    v-on

--- src/components/componeneteBlingTemplate.vue
+++ src/components/componeneteBlingTemplate.vue
@@ -4,10 +4,16 @@
     <h3>V-ON</h3>
     <button @click="saludar('anna')">Saludando</button>
     <input type="text" @input="mostrando($event)" />
+    <br />
+    <button @click.once="mostrarOne">solo una vez</button>
+    <br />
+    <input type="text" @keyup.enter="teclado" />
   </div>
 </template>
 <script setup>
 const saludar = (name) => console.log(`Hola mundo ${name}`)
 const mostrando = (event) => console.log(`Mostrando ${event.target.value}`)
+const mostrarOne = () => console.log('solo una vez')
+const teclado = () => console.log('Mostrando teclado')
 </script>
 <style scoped></style>
